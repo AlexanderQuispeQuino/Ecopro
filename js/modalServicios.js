2e6cfd830cc42006cf20fbfcd75224cb4bd7844b
AlexanderQuispeQuino <37464718+AlexanderQuispeQuino@users.noreply.github.com>
2018-05-15 19:05:20 -0500

    Add files via upload
    ecopro con sus modales , no es responsive aun

---
+++ js/modalServicios.js
@@ -0,0 +1,11 @@
+/*$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#servicio1").click(function(){
+        $("#myModalservicio1").modal();
+    });
+});*/
